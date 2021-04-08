e6d92e96dfd9c3a7474de9eb2c32032870f8bc79
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2021-04-08 12:37:19 +0100

    first commit

--- public/dessert.php
+++ public/dessert.php
@@ -1,39 +1,6 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<link rel="stylesheet" href="/styles.css"/>
-		<title>Kate's Kitchen - Dessert</title>
-	</head>
-	<body>
-	<header>
-		<section>
-			<aside>
-				<h3>Opening times:</h3>
-				<p>Sun-Thu: 12:00-22:00</p>
-				<p>Fri-Sat: 12:00-23:30</p>
-			</aside>
-			<h1>Kate's Kitchen</h1>
-
-		</section>
-	</header>
-	<nav>
-		<ul>
-			<li><a href="/">Home</a></li>
-			<li>Menu
-				<ul>
-					<li><a href="/starters.php">Starters</a></li>
-					<li><a href="/mains.php">Mains</a></li>
-					<li><a  href="/dessert.php">Dessert</a></li>
-
-				</ul>
-			</li>
-			<li><a href="/about.html">About Us</a></li>
-		</ul>
-
-	</nav>
-<img src="images/randombanner.php"/>
-
-	<main class="sidebar">
+<?php
+$title = 'Dessert';
+$content = '
 
 	<section class="left">
 		<ul>
@@ -74,13 +41,7 @@
 
 </ul>
 
-</section>
-	</main>
-
-
-	<footer>
-		&copy; Kate's Kitchen 2017
-	</footer>
-</body>
-</html>
+</section>';
+require layout.php;
+?>
 
