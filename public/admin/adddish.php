d162f0b133291b0dc194f6e8822f1903afba3a8d
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2021-04-11 21:44:01 +0100

    first commit

--- public/admin/adddish.php
+++ public/admin/adddish.php
@@ -31,17 +31,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 	}
 
 else {
-	?>
-	<h2>Log in</h2>
-
-	<form action="index.php" method="post">
-
-		<label>Password</label>
-		<input type="password" name="password" />
-
-		<input type="submit" name="submit" value="Log In" />
-	</form>
-<?php
+	require '../login.htm.php';
 }
 require '../templates/layout.html.php';
 
