b2581bf0672e6d55312a6824e0b702f716ac4a08
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2021-04-21 13:14:12 +0100

    first commit

---
+++ DatabaseTable.php
@@ -0,0 +1,57 @@
+<?php
+class DatabaseTable {
+    private $pdo;
+    private $table;
+    private $primaryKey;
+
+    public function __construct($pdo, $table, $primaryKey) {
+        $this->pdo = $pdo;
+        $this->table = $table;
+        $this->primaryKey = $primaryKey;
+    }
+
+
+public function findAll() {
+    $stmt = $this->pdo->prepare('SELECT * FROM ' . $this->table);
+    $stmt->execute(['value' => $value]);
+    return $stmt->fetchAll();
+}
+public function find($field, $value) {
+    $stmt = $this->pdo->prepare('SELECT * FROM' . $this->table . 'WHERE' . $field . ' = :value');
+    $stmt->execute(['value' => $value]);
+    return $stmt->fetchAll();
+}
+
+public function insert($record) {
+    $keys = array_keys($record);
+    $values = implode(', ', $keys);
+    $valuesWithColon = implode(', :', $keys);
+    $stmt = $this->pdo->prepare('INSERT INTO' . $this->table . '(' . $values . ') VALUES (:' . $valuesWithColon . ')');
+    $stmt->execute($record);
+}
+public function delete($field, $value) {
+    $stmt = $this->pdo->prepare('DELETE FROM' .  $this->table . 'WHERE' . $field . ' = :value');
+	$stmt->execute(['value' => $value]);
+}
+public function update($record) {
+
+    $query = 'UPDATE' . $this->table . 'SET ';
+    $parameters = [];
+    foreach($record as $key => $value) {
+        $parameters[] = $key . ' = :' . $key;
+    }
+    $query .= implode(', ', $parameters);
+    $query .= 'WHERE' . $this->primaryKey . ' = :primaryKey';
+    $record['primaryKey'] = $record[$this->primaryKey];
+    $stmt = $this->pdo->prepare($query);
+    $stmt->execute($record);
+}
+public function save($record) {
+    try {
+        $this->insert($record);
+    }
+    catch (Exception $e) {
+        $this->update($record);
+    }
+}
+}
