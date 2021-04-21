24f5e384c3764cd51adbc990bb226b3b2cea7477
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2021-04-21 15:10:26 +0100

    first commit

--- public/index.php
+++ public/index.php
@@ -1,6 +1,14 @@
 <?php
 require '../functions/loadTemplate.php';
 require '../dbconnection.php';
+require '../classes/Databasetable.php';
+
+$usersTable = new DatabaseTable($pdo, 'user', 'iduser');
+$dishesTable = new DatabaseTable($pdo, 'dish', 'id');
+$categoriesTable = new DatabaseTable($pdo, 'category', 'id');
+$reviewsTable = new DatabaseTable($pdo, 'review', 'idreview');
+
+
 require '../pages/' . $_GET['page'] . '.php';
 require '../templates/layout.html.php';
 ?>
