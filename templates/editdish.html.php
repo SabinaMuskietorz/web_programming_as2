7157cfc98aa124a1528343d3b143dd325b5fa827
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2021-05-15 23:09:09 +0100

    first commit

--- templates/editdish.html.php
+++ templates/editdish.html.php
@@ -1,6 +1,18 @@
 <form action="" method="POST">
 
     <input type="hidden" name="dish[id]" value="<?=$dish->id ?? ''?>"/>
+    <label>Select category:</label>
+	<select name="idcategory">Select category
+		<?php  
+        // loop through categories 
+		foreach ($categories as $category) { ?>
+        
+			<li><a href="/dish/edit?id=<?=$category->id?>"><?=$category->name?></a></li>
+            </ul>
+            <?php
+		}
+		?>
+	</select>
     <label>Name:</label>
     <input type="text" name="dish[name]" value="<?=$dish->name ?? ''?>"/>
     <label>Type description here:</label>
