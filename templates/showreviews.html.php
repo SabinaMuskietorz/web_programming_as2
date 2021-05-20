09ef6d113b7cb445b854cca031b449f4499a9ef1
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2021-05-20 21:37:38 +0100

    first commit

--- templates/showreviews.html.php
+++ templates/showreviews.html.php
@@ -1,6 +1,5 @@
 <section class="right">
 <h2><?= $dish->name; ?></h2>
-<a class="new" href="/review/edit">Add new review</a>
 
 <?php
 foreach ($reviews as $review) { 
@@ -9,12 +8,43 @@ foreach ($reviews as $review) {
     <li>
         <?=$review->reviewText?>
         <em>Posted by:  <?=$review->name?></em>
-        
+        <a href="/review/edit?id=<?=$review->id?>">edit</a>
         
         
     </li>
 </ul>
 
 <?php } ?>
+<!--<form action="/review/edit" method="POST">
+    <label>Name</label>
+    <input type="text" name="review[name]" value="" />
+    <input type="hidden" name="review[dishId]" value="" />
+    <label>Review</label>
+    <textarea name="review[reviewText]" placeholder="Type your review here"> </textarea>
+    <label>Rating 1-5</label>
+    <input type="text" name="review[rating]" value="" />
+    <input type="submit" value="Add" />
+</form>-->
+
+<form action="" method="POST">
+    <input type="hidden" name="review[id]" value="" />
+    <input type="hidden" name="review[dishId]" value="" />
+    <label>Name</label>
+    <input type="text" name="review[name]" value="" />
+    <label>Review</label>
+    <textarea name="review[reviewText]"></textarea>
+    <label>Rating 1-5</label>
+    <input type="text" name="review[rating]" value="" />
+    <?php
+    if(isset($_SESSION ['admin'])) {
+        ?>
+    <label>Visibility:</label>
+    <input type="text" name="review[visibility]" value="" />
+    <?php
+    }
+    ?>
+    <input type="submit" value="Save" />
+</form>
+
 
 
