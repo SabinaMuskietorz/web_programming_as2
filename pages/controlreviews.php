446b8b45de70649827df29e09e8963d321e39bc3
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2021-05-24 19:21:39 +0100

    first commit

--- pages/controlreviews.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../functions/loadTemplate.php';
-require '../dbconnection.php';
-$title = 'Manage reviews';
-$output = loadTemplate('../templates/reviewlist.html.php', []);
-require '../templates/layout.html.php';
-?>
-
